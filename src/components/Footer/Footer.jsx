ce13a6429ac6d1a76004bd9ae4edd6da3024483a
thanhhang31023 <thanhhang31023@gmail.com>
2025-03-26 12:27:08 +0900

    adding some more background noise

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+
+import { chill } from '../../data/songData';
+import './Footer.scss';
+import Player from '../Player/Player';
+
+const Footer = () => {
+  const [currentSongIndex, setCurrentSongIndex] = useState(1);
+
+  return (
+    <div className='footer'>
+      <div className='author'>
+        <span></span>
+      </div>
+      <div className='controller'>
+        <Player
+          currentSongIndex={currentSongIndex}
+          setCurrentSongIndex={setCurrentSongIndex}
+          songs={chill}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
