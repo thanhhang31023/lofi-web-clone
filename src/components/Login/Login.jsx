12d93509bcee2b7017c7b56790bbe42c7e2a73d9
thanhhang31023 <thanhhang31023@gmail.com>
2025-03-26 10:54:17 +0900

    working on login and header

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,20 +1,31 @@
 import React from 'react';
 
-import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link, Navigate } from 'react-router-dom';
+import { signInAPI } from '../../redux/actions';
 import './Login.scss';
 
 const Login = () => {
+  const data = useSelector((state) => state.userState);
+  const dispatch = useDispatch();
+  const { user } = data;
+
+  const signInHandler = () => {
+    dispatch(signInAPI());
+  };
+
   return (
     <div className='container'>
+      {user !== null && <Navigate to='home' />}
       <nav className='container__nav'>
-        <Link to='/'>
+        <Link to='/home'>
           <img src='/assets/icons/lofi-logo.gif' alt='' />
         </Link>
         <div className='nav-menu'>
           <a
             target='_blank'
             rel='noreferrer'
-            href='https://www.linkedin.com/'
+            href='https://www.linkedin.com'
           >
             <i className='fab fa-linkedin'></i>
             <span>Linkedin</span>
@@ -22,7 +33,7 @@ const Login = () => {
           <a
             target='_blank'
             rel='noreferrer'
-            href='https://github.com/'
+            href='https://github.com'
           >
             <i className='fab fa-github'></i>
             <span>GitHub</span>
@@ -38,10 +49,10 @@ const Login = () => {
         </div>
       </nav>
       <section className='container__section'>
-        <h1>Welcome to the auto genrate lofi music .</h1>
+        <h1>Welcome to the auto genrate lofi music</h1>
         <h1>Login to explore the feature</h1>
         <div className='form'>
-          <div className='icon'>
+          <div className='icon' onClick={signInHandler}>
             <img src='/assets/icons/google.svg' alt='' />
             Sign in with Google
           </div>
